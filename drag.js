7bec0491e39cf9e5f8e2e66fc34b7e09e6a65069
wq <wu2543279278@163.com>
2018-12-28 17:33:04 +0800

    drag

--- drag.js
+++ drag.js
@@ -1,5 +1,18 @@
 function drag(id){
 	var obj = document.getElementById(id);
-	obj.onmousedown = function(){
+	var disX =0;
+	var disY =0;
+	obj.onmousedown = function(ev){
+		disX = ev.pageX - obj.offsetLeft;
+		disY = ev.pageY - obj.offsetTop;
+		document.onmousemove = function(e){
+			obj.style.left = e.pageX - disX + 'px';
+			obj.style.top = e.pageY - disY +'px';
+		}
 	}
+	document.onmouseup = function(){
+		obj.onmousemove = null;
+		obj.onmouseup = null;
+	}
+	return false;
 }
